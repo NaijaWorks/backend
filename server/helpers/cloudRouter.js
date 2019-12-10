e123cc3892d9cf0aa925ae730e93e5fb49028fc2
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-11 05:40:05 +0800

    feat: define post to cloudinary route
    Co-authored-by: Oloruntobi Awoderu <45911584+OloruntobiAwoderu@users.noreply.github.com>
    Co-authored-by: Isaac Aderogba <37246355+IsaacAderogba@users.noreply.github.com>

---
+++ server/helpers/cloudRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const r = require('./responses');
+const { multerUploads, dataUri } = require('../middleware/multer');
+const { cloudinaryConfig, uploader } = require('../middleware/cloudinaryConfig');
+cloudinaryConfig(router);
+
+router.post('/v1/upload', multerUploads, async (req, res) => {
+   try {
+      if (req.file) {
+         const file = dataUri(req).content;
+         const { url } = await uploader.upload(file);
+         res.status(201).json({
+            message: r.uploadSuccess,
+            image: url
+         })
+      } else {
+         res.status(403).json(r.missing);
+      }
+   } catch (error) {
+      res.status(500).json(error.message);
+   }
+})
