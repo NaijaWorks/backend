b4d6a48a6ce0c6a1abcfc29c0d861180d3e464ee
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-13 16:29:22 +0800

    feat: implement restrict middleware improvement: update responses

--- server/helpers/responses.js
+++ server/helpers/responses.js
@@ -1,7 +1,8 @@
 module.exports = {
-   connected: (ip) =>  `Active connection to Server established via ${ip}`,
+   connected: (ip) => `Active connection to Server established via ${ip}`,
    userExists: "User with email already exists",
    invalid: "Invalid credentials",
+   supplyToken: 'Please supply token!',
    invalidID: "User with id not found",
    missing: "Please include file in the request",
    uploadSuccess: "Image successfully uploaded to Cloudinary"
--- server/helpers/tokenize.js
+++ server/helpers/tokenize.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const secret = require('../config/secrets');
 
+const User = require('../models/user');
+
 module.exports = {
    generateToken: function (user) {
       const payload = {
@@ -10,5 +12,14 @@ module.exports = {
          expiresIn: '30d'
       };
       return jwt.sign(payload, secret.jwtSecret, options)
+   },
+   restrict: async function (token) {
+      try {
+         const decodedToken = await jwt.verify(token, secret.jwtSecret);
+         const user = await User.findById(decodedToken.subject).exec();
+         return user;
+      } catch (error) {
+         return null;
+      }
    }
 }
