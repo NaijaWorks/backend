b31be25ee7e719cd1f61d000305d985474f21c58
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-14 22:57:37 +0100

    improvement: validate email arguments

--- server/helpers/responses.js
+++ server/helpers/responses.js
@@ -4,7 +4,10 @@ module.exports = {
    invalid: "Invalid credentials",
    hidden: "Oops! Why do you want to see the password?",
    supplyToken: 'Please supply token!',
+   supplyToken: 'Please supply token!',
+   invalidEmail: 'Not a valid email address format',
    invalidID: "User with id not found",
    missing: "Please include file in the request",
-   uploadSuccess: "Image successfully uploaded to Cloudinary"
+   uploadSuccess: "Image successfully uploaded to Cloudinary",
+   mailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 }
